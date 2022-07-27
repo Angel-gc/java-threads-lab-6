5ad06c49b39d2ae00b4d17a044b627ba87259e9e
Angel Concepcion <angelconcepcion50@gmail.com>
2022-07-27 11:57:33 -0400

    solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 import java.util.concurrent.*;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // create an executor
-        ExecutorService executor; 
+        ExecutorService executor = Executors.newSingleThreadExecutor();
 
         while (scanner.hasNext()) {
             int num = scanner.nextInt();
             // submit tasks to your executor
+            executor.submit(new PrimeLogger(num));
         }
     }
 }
@@ -24,5 +26,11 @@ class PrimeLogger implements Runnable {
     @Override
     public void run() {
         // print num if it is prime
+       boolean isPrime = IntStream.rangeClosed(2, num/2).noneMatch(i -> num%i == 0);
+       if (isPrime) {
+           System.out.println(num);
+       } else {
+           System.out.println(num + " is not a prime number.");
+       }
     }
 }
